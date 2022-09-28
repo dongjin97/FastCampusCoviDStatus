d54fc5e60489f39410dd7a8eeadbab7ba01c4b32
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2022-09-28 20:52:33 +0900

    dev/ing~

--- CovidStatusBoard/CovidStatusBoard/ViewController.swift
+++ CovidStatusBoard/CovidStatusBoard/ViewController.swift
@@ -4,16 +4,53 @@
 //
 //  Created by 원동진 on 2022/09/28.
 //
-
+//KW3gzmX6xGnwIpshdA7ifuycqtBa5UlDS
 import UIKit
-
+import Charts
+import Alamofire
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var totalCaseLabel: UILabel!
+    @IBOutlet weak var newCaseLabel: UILabel!
+    @IBOutlet weak var pieChartView: PieChartView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.fetchCovidOverview(completionHandler: { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(result):
+                debugPrint("success\(result)")
+            case let .failure(error):
+                debugPrint("error \(error)")
+            }
+            
+        })
+        
     }
-
-
+    func fetchCovidOverview(
+        completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
+    ) {
+        let url = "https://api.corona-19.kr/korea/country/new/"
+        let param = [
+            "serviceKey": "KW3gzmX6xGnwIpshdA7ifuycqtBa5UlDS"
+        ]
+        AF.request(url, method: .get, parameters: param)
+            .responseData(completionHandler: { response in
+                switch response.result {
+                case let .success(data):
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(CityCovidOverview.self, from: data)
+                        completionHandler(.success(result))
+                    } catch {
+                        completionHandler(.failure(error))
+                    }
+                case let .failure(error):
+                    completionHandler(.failure(error))
+                }
+            })
+    }
 }
+
+
 
