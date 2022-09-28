b8dd9352046c03db040d0882d26e4d1f72b3bb26
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2022-09-28 19:14:20 +0900

    dev/start

---
+++ CovidStatusBoard/CovidStatusBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidStatusBoard
+//
+//  Created by 원동진 on 2022/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
